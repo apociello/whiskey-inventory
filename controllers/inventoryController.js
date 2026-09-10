0ab4e0a0bf7226c0e50d421296f2f9f08bae95bb
pociello <asierpociello@gmail.com>
2026-09-10 07:11:11 +0200

    refactor: Add head partial

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,7 +2,7 @@ const db = require('../db/queries');
 
 const inventory = async (req, res) => {
   const whiskeys = await db.getWhiskeys();
-  res.render('index', { whiskeys });
+  res.render('index', { whiskeys: whiskeys, title: 'index', style: 'index' });
 };
 
 module.exports = {
